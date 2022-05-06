b67604c00b4a71907cf71f6c3f29272126f40238
kaitl <54618947+kaitl@users.noreply.github.com>
2022-05-07 05:42:49 +0800

    Update youdu.org.js

--- sources/youdu.org.js
+++ sources/youdu.org.js
@@ -6,7 +6,7 @@ const baseUrl = "https://www.yodu.org"
  * @returns {[{name, author, cover, detail}]}
  */
 const search = (key) => {
-  let response = POST(`${baseUrl}/sa`,{data:`searchkey=${key}&searchtype=all`,headers:["Cookie:jq_Obj=1; PHPSESSID=n1l42q7uvlqrv71s9r128q3vdk; jieqiUserInfo=jieqiUserId%3D26029%2CjieqiUserUname%3Djimmy53e%2CjieqiUserName%3Djimmy53e%2CjieqiUserGroup%3D3%2CjieqiUserGroupName%3D%E6%99%AE%E9%80%9A%E4%BC%9A%E5%91%98%2CjieqiUserVip%3D0%2CjieqiUserHonorId%3D2%2CjieqiUserHonor%3D%E5%91%86%E8%90%8C%2CjieqiUserToken%3D819e18ff55550e0787e5a8c547358cce%2CjieqiCodeLogin%3D0%2CjieqiCodePost%3D0%2CjieqiUserPassword%3D8c03d65956cb27e6168718b8adaac466%2CjieqiUserLogin%3D1638602979; jieqiVisitInfo=jieqiUserLogin%3D1638602979%2CjieqiUserId%3D26029; zh_choose=n; Hm_lvt_d6c21518da630dd4f86d47c04de176de=1638602981; __gads=ID=f3e210ccebb43de1-2261feb859cf0005:T=1638603441:RT=1638603441:S=ALNI_MZ8QwNZgsv88qprvm0WJivmtVIceA; jieqiVisitId=article_articleviews%3D9922%7C15277; Hm_lpvt_d6c21518da630dd4f86d47c04de176de=1638603444"]})
+  let response = POST(`${baseUrl}/sa`,{data:`searchkey=${key}&searchtype=all`})
   let array = []
   let $ = HTML.parse(response)
   if ($('[name=keywords]').attr('content') == "图书搜索,文学搜索,小说搜索,有度中文网") {
@@ -97,5 +97,6 @@ const chapter = (url) => {
 var bookSource = JSON.stringify({
   name: "有度中文",
   url: "youdu.org",
-  version: 101
+  version: 102,
+  authorization: "https://www.yodu.org/login.php"
 })
